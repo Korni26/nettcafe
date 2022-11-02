c1209cbc5fe4f5056028fcc2be1d33ddb82a3e6f
Korni26 <thewolron@gmail.com>
2022-11-02 13:37:07 +0100

    first commit

---
+++ adminSide.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+<a href="index.php">
+<div class="logo"></div>
+    </a>
+    <div class="topBar">
+	<a href="loggInPage.php">
+        <div class="loggInBtn"></div>
+		</a>
+    </div>
+    <div class="menuBox">
+        <div class="adminOpt1">
+            <a href="">Legg til product</a>
+        </div>
+    </div>
+    <div class="contentBox">
+    <div class="row">
+</body>
+
+<?php
+
+        $dbc = mysqli_connect('localhost', 'root', '', 'adminbrukere')
+          or die('Error connecting to MySQL server.');
+
+        $query = "SELECT id, productName, productDescription, price from products";
+
+        $result = mysqli_query($dbc, $query)
+            or die('Error querying database.');
+
+        mysqli_close($dbc);
+
+        foreach($result as $row){
+            echo '<div class="product">';
+            echo '<div class="productPicture"></div>';
+            echo '<div class="productDescription">';
+                echo '<div class="prouctText">';
+                    echo '<h2 class="productName">';
+                    echo $row['productName'];
+                    echo '</h2>';
+                    echo '<div class="aboutProduct">';
+                        echo '<p>';
+                        echo $row['productDescription'];
+                        echo '</p>';
+                    echo '</div>';
+                echo '</div>';
+                echo '<div class="addCartBtnWrap"><button class="addToCartBtn">add to cart</button>';
+                echo '</div>';
+            echo '</div>';
+        echo '</div>';
+
+
+
+
+            // echo "<tr>";
+
+            // echo "<td>";
+            // echo $row['id'];
+            // echo "</td>";
+
+            // echo "<td>";    
+            // echo $row['productName'];
+            // echo "</td>";
+            
+            // echo "<td>";
+            // echo $row['productDescription'];
+            // echo "</td>";
+            
+            // echo "<td>";
+            // echo $row['price'];
+            // echo "</td>";
+
+            // echo "</tr>";
+        }
+?>
+
+</div>
+
+</html>
---
+++ loggInPage.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <a href="index.php">
+        <div class="logo"></div>
+    </a>
+    <div class="topBar">
+	<a href="loggInPage.php">
+        <div class="loggInBtn"></div>
+	</a>
+    </div>
+    <div class="menuBox"></div>
+        <div class="contentBox">
+                <form class="loggInForm" method="post">
+                    <br>
+                    <label for="brukernavn">Brukernavn</label>
+                    <input type="text" name="brukernavn">
+                    <br>
+                    <label for="passord">Passord</label>
+                    <input type="password" name="passord" id="passord">
+                    <div class="check">
+                    <!-- <input type="checkbox"> -->
+                    <!-- <label for="checkbox">Se passord</label> -->
+                    </div>
+                    <br>
+                    <button type="submit" name="submit">Send</button>
+                </form>
+        </div>
+        <script src="main.js"></script>
+</body>
+<?php
+if(isset($_POST['submit'])){
+    $brukernavn = $_POST['brukernavn'];
+    $passord = $_POST['passord'];
+
+    $dbc = mysqli_connect('localhost', 'root', '', 'adminbrukere')
+        or die('Error connecting to MySQL server');
+
+    $query = "SELECT brukernavn, passord from adminuser where brukernavn='$brukernavn' and passord='$passord'";
+
+    $result = mysqli_query($dbc, $query)
+        or die('Error querying database.');
+
+    mysqli_close($dbc);
+
+    // var_dump($result);
+
+    if($result->num_rows > 0){
+        header('location: adminSide.php');
+        var_dump('bra');
+    }else{
+        header('location: ikkeBra.html');
+        var_dump('ikke bra');
+    }
+}
+
+
+
+?>
+</html>
